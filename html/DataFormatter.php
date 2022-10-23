d5b0bcab22cbb0fb8423c35c7c858afa9b17c372
freeman <4reeman>
2022-10-23 15:19:15 +0300

    auto run

--- html/DataFormatter.php
+++ html/DataFormatter.php
@@ -48,7 +48,7 @@ class DataFormatter {
             $response->response('sendMessage', $trelloButton);
         }
         else {
-            $response->response('sendMessage',[$this->getChatId(), hex2bin('\xE2\x98\x9D')]);
+            $response->response('sendMessage',[$this->getChatId(), utf8_encode('\xE2\x98\x9D')]);
         }
     }
 
