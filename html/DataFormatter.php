b466b06a171d17aa249143240bd831c80a009ac5
freeman <4reeman>
2022-10-24 04:36:00 +0300

    auto run

--- html/DataFormatter.php
+++ html/DataFormatter.php
@@ -72,7 +72,7 @@ class DataFormatter {
                         'reply_markup' => $encodedKeyboard,
                     ];
                     $response->response('sendMessage', $trelloTokenLink);
-                $url_q = 'https://api.trello.com/1/members/me/boards?key=' . $this->api_key .'&token=' . '1b5ccb13c4da29a0e24e72265d1f5288784712017cadb158b65920d1c85980b1;'
+                $url_q = 'https://api.trello.com/1/members/me/boards?key=' . $this->api_key .'&token=' . '1b5ccb13c4da29a0e24e72265d1f5288784712017cadb158b65920d1c85980b1';
                 $header = get_headers($url_q);
                 $status_code = $header[0];
                 $response->response('sendMessage', ['chat_id'=>$this->getChatId(), 'text' =>$status_code]);
