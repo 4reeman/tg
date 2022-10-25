01c538da6a679e631f53d7b023d32bff530f53b5
freeman <4reeman>
2022-10-25 20:18:31 +0300

    auto run

--- html/index.php
+++ html/index.php
@@ -150,11 +150,11 @@ catch (PDOException $e) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-<div class = 'main'>
-    <form id="validation">
+<div class='main'>
+    <form id="validation" class="form_validation">
         <label for="api_key">Please, enter Your Key:</label>
         <input type="text" id="api_key">
-        <input type="button" id="submit_btn" value="Submit">
+        <input type="button" id="submit_btn" class="submit" value="Submit">
     </form>
 </div>
 <script src="dist/js/index.min.js"></script>
