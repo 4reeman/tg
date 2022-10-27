bb93771206165fcc76c76aa18a1fb83357eb78c0
freeman <4reeman>
2022-10-27 16:45:18 +0300

    auto run

--- App/assets/js/index.js
+++ App/assets/js/index.js
@@ -4,19 +4,30 @@ let btn = $('#submit_btn');
 function sendData() {
     let key =  $('#api_key').val()
     $.ajax({
-        url: 'https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=' . key,
-        type: "get",
-        data: "",
-        success: function() {
-            alert("Done, look at your console's network tab");
+        url: "https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=" . key,
+        type: "GET",
+        data: {},
+        dataType:"text json",
+        success: function(json, textStatus, xhr) {
+            console.log(arguments);
+            console.log(xhr.status);
         },
-        error: function(jqXHR, textStatus, errorThrown){
-            // log the error to the console
-            console.log(
-                "The following error occured: "+
-                textStatus, errorThrown
-            );
+        complete: function(xhr, textStatus) {
+            console.log(xhr.status);
         }
+        // url: 'https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read&response_type=token&key=' . key,
+        // type: "get",
+        // data: "",
+        // success: function() {
+        //     alert("Done, look at your console's network tab");
+        // },
+        // error: function(jqXHR, textStatus, errorThrown){
+        //     // log the error to the console
+        //     console.log(
+        //         "The following error occured: "+
+        //         textStatus, errorThrown
+        //     );
+        // }
     });
 }
 btn.on('click', ()=>{
