56b2ea2744468e132cf4e77a4386a26e5b511180
freeman <4reeman>
2022-10-27 14:49:08 +0300

    auto run

--- App/assets/js/index.js
+++ App/assets/js/index.js
@@ -23,17 +23,17 @@ btn.on('click', ()=>{
    sendData();
 });
 
-let accordion = $('.accordion');
-
-for (let i = 0; i < accordion.length; i++) {
-    console.log(accordion[i]);
-    accordion[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
+// let accordion = $('.accordion');
+//
+// for (let i = 0; i < accordion.length; i++) {
+//     console.log(accordion[i]);
+//     accordion[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//             panel.style.maxHeight = null;
+//         } else {
+//             panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
