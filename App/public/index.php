92c6ddec508b3efb68e291caf9c4a8f04615acb5
freeman <4reeman>
2022-10-29 17:15:15 +0300

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -7,10 +7,20 @@ $a = new DataFormatter();
 //$b = new PostHook();
 //$mon = $b->getData();
 
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
 function a() {
     $data=json_decode(file_get_contents("php://input"),true, 20, JSON_OBJECT_AS_ARRAY);
     if($data!=null) {
-        file_put_contents('data.json', json_encode($data));
+        file_put_contents('data.json', json_encode(getallheaders()));
 //        refresh( 2 );
     }
     else {
