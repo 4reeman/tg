66aed9a2c8ce943147a0cadcac68d9893d98060f
freeman <4reeman>
2022-10-31 06:32:22 +0200

    auto run

--- App/src/DataFormatter.php
+++ App/src/DataFormatter.php
@@ -45,16 +45,19 @@ class DataFormatter {
 //                        ]
 //                    ]
 //                ]
-            'inline_keyboard' => [
-                [
-                    'login_url' => [
-                        'url' => 'https://oauth.telegram.org/auth?bot_id=5793129764&origin=https%3A%2F%2Fserver4reema.vps.webdock.cloud&embed=1&request_access=write&return_to=https%3A%2F%2Fserver4reema.vps.webdock.cloud%2F',
-                        'forward_text' => 'saf'
-//                        'request_write_access' => true
-                    ]
-                ]
+                'inline_keyboard' => [[
+                    [
+                        'text' => urlencode("login \xE2\xAD\x90"),
+                        'callback_data' => 'log'
+                    ],
+                    [
+                        'text' => 'Login to the website',
+            'login_url' => [
+                'url' => 'https://server4reema.vps.webdock.cloud/index.php',
+                'request_write_access' => true
             ]
-
+       ]
+    ]]
 
 
 
