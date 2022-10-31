e4b852e1c2a0e35cff75d61c30fd93f69b98fcb2
freeman <4reeman>
2022-10-31 06:01:20 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -45,6 +45,9 @@ function refresh($time){
 //    echo "</pre>";
 }
 a();
+$BOT_USERNAME = 'custom_freeman_bot';
+$BOT_TOKEN = '5793129764:AAGR9DRRbMjBl4Byei70Sec6OiqAfuwdQRw';
+$REDIRECT_URI = 'https://server4reema.vps.webdock.cloud/';
 ?>
 
 <!DOCTYPE html>
@@ -56,6 +59,7 @@ a();
     <title>Trello Authorization</title>
     <link rel="stylesheet" href="dist/css/style.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://telegram.org/js/telegram-widget.js?2" data-telegram-login="<?= $BOT_USERNAME ?>" data-size="medium" data-auth-url="<?= $REDIRECT_URI ?>" data-request-access="write"></script>
 </head>
 <body>
 <div class='main'>
