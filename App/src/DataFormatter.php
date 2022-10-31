fa76a09f1be09a7079d982417baaf5f3911cba47
freeman <4reeman>
2022-11-01 00:54:14 +0200

    auto run

--- App/src/DataFormatter.php
+++ App/src/DataFormatter.php
@@ -31,19 +31,19 @@ class DataFormatter {
         if ($this->getMessage() == '/start') {
             $response->response('sendMessage', ['chat_id' => $this->getChatId(), 'text' => 'Hi, ' . $this->getUserName()]);
             $db = new SqlDatabaseConnection();
-            $tgData = [
-                'user_id' => $this->getUserId(),
-                'chat_id' => $this->getChatId()
-            ];
-//            $tgInsertData = [
+//            $tgData = [
 //                'user_id' => $this->getUserId(),
-//                'user_name' => $this->getUserName(),
 //                'chat_id' => $this->getChatId()
 //            ];
-            if (!$db->reviewData($tgData)) {
-                echo 'reviewed';
-//                 $db->insertData($tgInsertData);
-            }
+            $tgInsertData = [
+                'user_id' => $this->getUserId(),
+                'user_name' => $this->getUserName(),
+                'chat_id' => $this->getChatId()
+            ];
+//            if (!$db->reviewData($tgData)) {
+//                echo 'reviewed';
+                 $db->insertData($tgInsertData);
+//            }
             $response->response('sandMessage', ['chat_id' => $this->getChatId(), 'pls, copy your personal key']);
             $keyboard = [
                 'inline_keyboard' => [
