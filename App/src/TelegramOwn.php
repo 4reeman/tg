de361c0b920edf01ec5e72725ca3596dfa556fb5
freeman <4reeman>
2022-11-01 19:21:09 +0200

    auto run

--- App/src/TelegramOwn.php
+++ App/src/TelegramOwn.php
@@ -4,6 +4,10 @@ class TelegramOwn implements InputDataInterface {
 
     public $data;
 
+    public function __construct() {
+        $this->getData();
+    }
+
     public function getData() {
         $http_body = file_get_contents("php://input");  // exploit change! don`t use file_get_contents
         $data = json_decode($http_body,true, 25, JSON_OBJECT_AS_ARRAY);
