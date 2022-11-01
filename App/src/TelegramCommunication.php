e03ecc7278d37225491bcd33b515fa2a0320e865
freeman <4reeman>
2022-11-01 19:23:29 +0200

    auto run

--- App/src/TelegramCommunication.php
+++ App/src/TelegramCommunication.php
@@ -10,7 +10,6 @@ class TelegramCommunication {
         $this->data = $data;
         $this->response = $response;
         $this->db = $db;
-        $this->communicate();
     }
 
     public function communicate() {
@@ -27,39 +26,39 @@ class TelegramCommunication {
 
     public function startResponse() {
         $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => 'Hi, ' . $this->data->getUserName()]);
-//        $tgData = [
-//            'user_id' => $this->data->getUserId(),
-//            'chat_id' => $this->data->getChatId()
-//        ];
-//        $tgInsertData = [
-//            'user_id' => $this->data->getUserId(),
-//            'user_name' => $this->data->getUserName(),
-//            'chat_id' => $this->data->getChatId()
-//        ];
-//        if(!$this->db->reviewData($tgData)) {
-//            $this->db->insertData($tgInsertData);
-//        }
-//        $keyboard = [
-//            'inline_keyboard' => [
-//                [
-//                    [
-//                        'text' => 'Trello Authorization',
-//                        'login_url' => [
-//                            'url' => 'https://server4reema.vps.webdock.cloud/trello',
-//                            'request_write_access' => true,
-//                            'forward_text' => 'Login (forwarded)'
-//                        ]
-//                    ]
-//                ]
-//            ]
-//        ];
-//        $encodedKeyboard = json_encode($keyboard);
-//        $trelloKeyLink = [
-//            'chat_id' => $this->data->getChatId(),
-//            'text' => 'You need to connect your Telegram account ' .
-//                'with our server to use the functionality of this bot in full',
-//            'reply_markup' => $encodedKeyboard,
-//        ];
-//        $this->response->send('sendMessage', $trelloKeyLink);
+        $tgData = [
+            'user_id' => $this->data->getUserId(),
+            'chat_id' => $this->data->getChatId()
+        ];
+        $tgInsertData = [
+            'user_id' => $this->data->getUserId(),
+            'user_name' => $this->data->getUserName(),
+            'chat_id' => $this->data->getChatId()
+        ];
+        if(!$this->db->reviewData($tgData)) {
+            $this->db->insertData($tgInsertData);
+        }
+        $keyboard = [
+            'inline_keyboard' => [
+                [
+                    [
+                        'text' => 'Trello Authorization',
+                        'login_url' => [
+                            'url' => 'https://server4reema.vps.webdock.cloud/trello',
+                            'request_write_access' => true,
+                            'forward_text' => 'Login (forwarded)'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $encodedKeyboard = json_encode($keyboard);
+        $trelloKeyLink = [
+            'chat_id' => $this->data->getChatId(),
+            'text' => 'You need to connect your Telegram account ' .
+                'with our server to use the functionality of this bot in full',
+            'reply_markup' => $encodedKeyboard,
+        ];
+        $this->response->send('sendMessage', $trelloKeyLink);
     }
 }
