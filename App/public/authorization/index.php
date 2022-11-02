26503eef3ed9ab3d7cbc633729499d40430c11d5
freeman <4reeman>
2022-11-02 18:11:05 +0200

    auto run

--- App/public/authorization/index.php
+++ App/public/authorization/index.php
@@ -4,37 +4,37 @@ var_dump($_GET);
 if ($_SERVER["QUERY_STRING"] == null){
     echo 'url empty';
 }
-function checkTelegramAuthorization($auth_data)
-{
-    $check_hash = $auth_data['hash'];
-    unset($auth_data['hash']);
-    $data_check_arr = [];
-    foreach ($auth_data as $key => $value)
-    {
-        $data_check_arr[] = $key . '=' . $value;
-    }
-    sort($data_check_arr);
-    $data_check_string = implode("\n", $data_check_arr);
-    $secret_key = hash('sha256', BOT_TOKEN, true);
-    $hash = hash_hmac('sha256', $data_check_string, $secret_key);
-//    if (strcmp($hash, $check_hash) !== 0)
+//function checkTelegramAuthorization($auth_data)
+//{
+//    $check_hash = $auth_data['hash'];
+//    unset($auth_data['hash']);
+//    $data_check_arr = [];
+//    foreach ($auth_data as $key => $value)
 //    {
-//        print_r($_GET);
-//        throw new Exception('Data is NOT from Telegram') ;
+//        $data_check_arr[] = $key . '=' . $value;
 //    }
-//    if ((time() - $auth_data['auth_date']) > 86400)
-//    {
-//        throw new Exception('Data is outdated');
-//    }
-
-    return $auth_data;
-}
-
-function saveTelegramUserData($auth_data)
-{
-    $auth_data_json = json_encode($auth_data);
+//    sort($data_check_arr);
+//    $data_check_string = implode("\n", $data_check_arr);
+//    $secret_key = hash('sha256', BOT_TOKEN, true);
+//    $hash = hash_hmac('sha256', $data_check_string, $secret_key);
+////    if (strcmp($hash, $check_hash) !== 0)
+////    {
+////        print_r($_GET);
+////        throw new Exception('Data is NOT from Telegram') ;
+////    }
+////    if ((time() - $auth_data['auth_date']) > 86400)
+////    {
+////        throw new Exception('Data is outdated');
+////    }
+//
+//    return $auth_data;
+//}
+//
+//function saveTelegramUserData($auth_data)
+//{
+//    $auth_data_json = json_encode($auth_data);
 //    setcookie('tg_user', $auth_data_json);
-}
+//}
 
 //    print_r($_GET);
 //    var_export($_GET);
