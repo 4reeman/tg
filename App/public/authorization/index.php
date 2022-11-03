4ee23c2f15a8d2536cb9c502f21a8b4a9983fc62
freeman <4reeman>
2022-11-03 21:24:27 +0200

    auto run

--- App/public/authorization/index.php
+++ App/public/authorization/index.php
@@ -8,7 +8,6 @@ define('BOT_TOKEN', '5793129764:AAGR9DRRbMjBl4Byei70Sec6OiqAfuwdQRw'); // place 
 //    echo 'url empty';
 //}
 //phpinfo();
-print_r($_GET);
 function checkTelegramAuthorization($auth_data)
 {
     $check_hash = $auth_data['hash'];
@@ -22,15 +21,12 @@ function checkTelegramAuthorization($auth_data)
     $data_check_string = implode("\n", $data_check_arr);
     $secret_key = hash('sha256', BOT_TOKEN, true);
     $hash = hash_hmac('sha256', $data_check_string, $secret_key);
-//    if (strcmp($hash, $check_hash) !== 0)
-//    {
-//        print_r($_GET);
-//        throw new Exception('Data is NOT from Telegram') ;
-//    }
-//    if ((time() - $auth_data['auth_date']) > 86400)
-//    {
-//        throw new Exception('Data is outdated');
-//    }
+    if (strcmp($hash, $check_hash) !== 0) {
+        throw new Exception('Data is NOT from Telegram') ;
+    }
+    if ((time() - $auth_data['auth_date']) > 86400) {
+        throw new Exception('Data is outdated');
+    }
 
     return $auth_data;
 }
@@ -41,13 +37,17 @@ function saveTelegramUserData($auth_data)
     setcookie('tg_user', $auth_data_json);
 }
 
-    print_r($_GET);
-    var_export($_GET);
-    print_r($_SERVER['QUERY_STRING']);
-    var_export($_SERVER['QUERY_STRING']);
-    checkTelegramAuthorization($_GET);
+try
+{
+    $auth_data = checkTelegramAuthorization($_GET);
+    echo $auth_data;
 //    saveTelegramUserData($auth_data);
+}
+catch(Exception $e)
+{
+    die($e->getMessage());
+}
 
-//header('Location: index.php');
+//header('Location: /trello/index.php');
 ?>
 
