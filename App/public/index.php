69e6ff91073cde5e44bce92e5ecc15633b519274
freeman <4reeman>
2022-11-04 01:05:56 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -9,6 +9,29 @@ include '../src/TelegramOwn.php';
 include '../src/SendMessage.php';
 include '../src/SqlDatabaseConnection.php';
 
+function a() {
+    $data=json_decode(file_get_contents("php://input"),true, 20, JSON_OBJECT_AS_ARRAY);
+    if($data!=null) {
+        file_put_contents('data.json', json_encode($data));
+        refresh( 100 );
+    }
+    else {
+        echo (is_object($data));
+        refresh( 100);
+    }
+}
+function refresh($time){
+    $current_url = $_SERVER[ 'REQUEST_URI' ];
+    header( "Refresh: " . $time . "; URL=$current_url" );
+    $ret = json_decode(file_get_contents('data.json'), true);
+    echo $ret['message']['from']['first_name'];
+    echo $ret['message']['from']['id'];
+    echo "<pre>";
+    print_r($ret);
+    echo "</pre>";
+}
+a();
+
 $header = 'telegram';
 
 if($header == 'telegram') {
