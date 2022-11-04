62aea5c00f36b50af39d39bedc78102ffa8836a7
freeman <4reeman>
2022-11-04 17:32:23 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -14,7 +14,7 @@ function a() {
 //    $data = json_encode($_SERVER);
     if($data!=null) {
 //        file_put_contents('data.json', json_encode($data));
-        file_put_contents('data.json', json_encode($_POST));
+        file_put_contents('data.json', json_encode($_SERVER));
         file_put_contents('data1.json', json_encode(getallheaders()));
         refresh( 3 );
     }
