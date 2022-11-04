0e56824664b727033413dfc3e1a73dd786a0bfd5
freeman <4reeman>
2022-11-04 17:19:58 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -10,10 +10,11 @@ include '../src/SendMessage.php';
 include '../src/SqlDatabaseConnection.php';
 
 function a() {
-    $data=json_decode(file_get_contents("php://input"),true, 20, JSON_OBJECT_AS_ARRAY);
+//    $data=json_decode(file_get_contents("php://input"),true, 20, JSON_OBJECT_AS_ARRAY);
+    $data = json_encode($_POST);
     if($data!=null) {
 //        file_put_contents('data.json', json_encode($data));
-        file_put_contents('data.json', $_POST);
+        file_put_contents('data.json', json_encode($_POST));
         file_put_contents('data1.json', json_encode(getallheaders()));
         refresh( 3 );
     }
