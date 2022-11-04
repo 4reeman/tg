178a586c9020f927b931a5817dbf95bb5e7a9a09
freeman <4reeman>
2022-11-04 16:01:00 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -11,8 +11,10 @@ include '../src/SqlDatabaseConnection.php';
 
 function a() {
     $data=json_decode(file_get_contents("php://input"),true, 20, JSON_OBJECT_AS_ARRAY);
+    $bla = $_POST;
+    $bla_js = json_encode($bla);
     if($data!=null) {
-        file_put_contents('data.json', json_encode($_POST));
+        file_put_contents('data.json', $bla_js);
 //        file_put_contents('data1.json', json_encode(getallheaders()));
         refresh( 3 );
     }
@@ -25,9 +27,10 @@ function refresh($time){
     $current_url = $_SERVER[ 'REQUEST_URI' ];
     header( "Refresh: " . $time . "; URL=$current_url" );
     $ret = json_decode(file_get_contents('data.json'), true);
+    $bla_js1 = file_get_contents('data.json');
 //    $ret1 = json_decode(file_get_contents('data1.json'), true);
     var_dump($ret);
-//    var_dump($ret1);
+    var_dump($bla_js1);
 }
 a();
 
