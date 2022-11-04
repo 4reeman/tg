4efb5606c35e6746ca45f584f7c6de5f7f19e676
freeman <4reeman>
2022-11-04 16:52:23 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -12,9 +12,9 @@ include '../src/SqlDatabaseConnection.php';
 function a() {
     $data=json_decode(file_get_contents("php://input"),true, 20, JSON_OBJECT_AS_ARRAY);
     if($data!=null) {
-        file_put_contents('data.json', json_encode($data));
-//        file_put_contents('data.json', json_encode($_SERVER));
-//        file_put_contents('data1.json', json_encode(getallheaders()));
+//        file_put_contents('data.json', json_encode($data));
+        file_put_contents('data.json', json_encode($_SERVER));
+        file_put_contents('data1.json', json_encode(getallheaders()));
         refresh( 3 );
     }
     else {
@@ -26,10 +26,9 @@ function refresh($time){
     $current_url = $_SERVER[ 'REQUEST_URI' ];
     header( "Refresh: " . $time . "; URL=$current_url" );
     $ret = json_decode(file_get_contents('data.json'), true);
-    $bla_js1 = file_get_contents('data.json');
-//    $ret1 = json_decode(file_get_contents('data1.json'), true);
+    $ret1 = json_decode(file_get_contents('data1.json'), true);
     var_dump($ret);
-    var_dump($bla_js1);
+    var_dump($ret1);
 }
 a();
 
