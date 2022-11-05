f8568fea01483daa81088b8c0503a5db2369d620
freeman <4reeman>
2022-11-05 15:43:22 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -11,54 +11,51 @@ include '../src/SqlDatabaseConnection.php';
 include '../src/TrelloAuthorizationService.php';
 include '../src/TrelloAuthorizationCommunication.php';
 //
-//$headers = getallheaders();
-function a() {
-    $data=json_decode(file_get_contents("php://input"));
-//    $data = json_encode($_SERVER);
-    if($data!=null) {
-        file_put_contents('data.json', json_encode($data));
-        file_put_contents('data1.json', json_encode(getallheaders()));
-        file_put_contents('data2.json', json_encode($_SERVER));
-        refresh( 3 );
-    }
-    else {
-        echo (is_object($data));
-        refresh( 3);
-    }
-}
-function refresh($time){
-    $current_url = $_SERVER[ 'REQUEST_URI' ];
-    header( "Refresh: " . $time . "; URL=$current_url" );
-    $ret = json_decode(file_get_contents('data.json'), true);
-    $ret1 = json_decode(file_get_contents('data1.json'), true);
-    $ret2 = json_decode(file_get_contents('data2.json'), true);
-    echo ' </br>';
-    echo "<pre>";
-    print_r($ret);
-    echo "</pre>";
-    echo ' </br>';
-    echo "<pre>";
-    print_r($ret1);
-    echo "</pre>";
-    echo ' </br>';
-    echo "<pre>";
-    print_r($ret2);
-    echo "</pre>";
-}
-a();
-//$headers = [
-//    "Source" => 'trello_authorization'
-//];
+$headers = getallheaders();
+//function a() {
+//    $data=json_decode(file_get_contents("php://input"));
+////    $data = json_encode($_SERVER);
+//    if($data!=null) {
+//        file_put_contents('data.json', json_encode($data));
+//        file_put_contents('data1.json', json_encode(getallheaders()));
+//        file_put_contents('data2.json', json_encode($_SERVER));
+//        refresh( 3 );
+//    }
+//    else {
+//        echo (is_object($data));
+//        refresh( 3);
+//    }
+//}
+//function refresh($time){
+//    $current_url = $_SERVER[ 'REQUEST_URI' ];
+//    header( "Refresh: " . $time . "; URL=$current_url" );
+//    $ret = json_decode(file_get_contents('data.json'), true);
+//    $ret1 = json_decode(file_get_contents('data1.json'), true);
+//    $ret2 = json_decode(file_get_contents('data2.json'), true);
+//    echo ' </br>';
+//    echo "<pre>";
+//    print_r($ret);
+//    echo "</pre>";
+//    echo ' </br>';
+//    echo "<pre>";
+//    print_r($ret1);
+//    echo "</pre>";
+//    echo ' </br>';
+//    echo "<pre>";
+//    print_r($ret2);
+//    echo "</pre>";
+//}
+//a();
 $tg_header = 'telegram';
 
-if($tg_header == 'telegram') {
+if($headers["HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN"] == 'telegram') {
     $tg = new TelegramCommunication(new TelegramOwn(), new SendMessage(), new SqlDatabaseConnection());
     $tg->communicate();
 }
-//
-//if($headers["Source"] == 'trello_authorization') {
-//    $authorization = new TrelloAuthorizationCommunication(new TrelloAuthorizationService(), new SendMessage(), new SqlDatabaseConnection());
-//    $authorization->insertKey();
-//}
+
+if($headers["Source"] == 'trello_authorization') {
+    $authorization = new TrelloAuthorizationCommunication(new TrelloAuthorizationService(), new SendMessage(), new SqlDatabaseConnection());
+    $authorization->insertKey();
+}
 
 
