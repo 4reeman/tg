0a7e2e8687e21b78ef67b4c3d188e1470cb20e45
freeman <4reeman>
2022-11-05 15:58:55 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -13,39 +13,39 @@ include '../src/TrelloAuthorizationCommunication.php';
 //
 $data=json_decode(file_get_contents("php://input"));
 $headers = getallheaders();
-//function a() {
-////    $data=json_decode(file_get_contents("php://input"));
+function a() {
+    $data=json_decode(file_get_contents("php://input"));
 //    $data = json_encode($_POST);
-//    if($data!=null) {
-//        file_put_contents('data.json', json_encode($data));
-//        file_put_contents('data1.json', json_encode(getallheaders()));
-//        file_put_contents('data2.json', json_encode($_POST));
-//        refresh( 3 );
-//    }
-//    else {
-//        refresh( 3);
-//    }
-//}
-//function refresh($time){
-//    $current_url = $_SERVER[ 'REQUEST_URI' ];
-//    header( "Refresh: " . $time . "; URL=$current_url" );
-//    $ret = json_decode(file_get_contents('data.json'), true);
-//    $ret1 = json_decode(file_get_contents('data1.json'), true);
-//    $ret2 = json_decode(file_get_contents('data2.json'), true);
-//    echo ' </br>';
-//    echo "<pre>";
-//    print_r($ret);
-//    echo "</pre>";
-//    echo ' </br>';
-//    echo "<pre>";
-//    print_r($ret1);
-//    echo "</pre>";
-//    echo ' </br>';
-//    echo "<pre>";
-//    print_r($ret2);
-//    echo "</pre>";
-//}
-//a();
+    if($data!=null) {
+        file_put_contents('data.json', json_encode($data));
+        file_put_contents('data1.json', json_encode(getallheaders()));
+        file_put_contents('data2.json', json_encode($_POST));
+        refresh( 3 );
+    }
+    else {
+        refresh( 3);
+    }
+}
+function refresh($time){
+    $current_url = $_SERVER[ 'REQUEST_URI' ];
+    header( "Refresh: " . $time . "; URL=$current_url" );
+    $ret = json_decode(file_get_contents('data.json'), true);
+    $ret1 = json_decode(file_get_contents('data1.json'), true);
+    $ret2 = json_decode(file_get_contents('data2.json'), true);
+    echo ' </br>';
+    echo "<pre>";
+    print_r($ret);
+    echo "</pre>";
+    echo ' </br>';
+    echo "<pre>";
+    print_r($ret1);
+    echo "</pre>";
+    echo ' </br>';
+    echo "<pre>";
+    print_r($ret2);
+    echo "</pre>";
+}
+a();
 
 if($headers["HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN"] == 'telegram') {
     $tg = new TelegramCommunication(new TelegramOwn(), new SendMessage(), new SqlDatabaseConnection());
