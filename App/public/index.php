14c24fa666ec8db01b53c013f1c1b7855155a166
freeman <4reeman>
2022-11-05 15:55:22 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -11,40 +11,41 @@ include '../src/SqlDatabaseConnection.php';
 include '../src/TrelloAuthorizationService.php';
 include '../src/TrelloAuthorizationCommunication.php';
 //
+$data=json_decode(file_get_contents("php://input"));
 $headers = getallheaders();
-function a() {
-//    $data=json_decode(file_get_contents("php://input"));
-    $data = json_encode($_POST);
-    if($data!=null) {
-        file_put_contents('data.json', json_encode($data));
-        file_put_contents('data1.json', json_encode(getallheaders()));
-        file_put_contents('data2.json', json_encode($_POST));
-        refresh( 3 );
-    }
-    else {
-        refresh( 3);
-    }
-}
-function refresh($time){
-    $current_url = $_SERVER[ 'REQUEST_URI' ];
-    header( "Refresh: " . $time . "; URL=$current_url" );
-    $ret = json_decode(file_get_contents('data.json'), true);
-    $ret1 = json_decode(file_get_contents('data1.json'), true);
-    $ret2 = json_decode(file_get_contents('data2.json'), true);
-    echo ' </br>';
-    echo "<pre>";
-    print_r($ret);
-    echo "</pre>";
-    echo ' </br>';
-    echo "<pre>";
-    print_r($ret1);
-    echo "</pre>";
-    echo ' </br>';
-    echo "<pre>";
-    print_r($ret2);
-    echo "</pre>";
-}
-a();
+//function a() {
+////    $data=json_decode(file_get_contents("php://input"));
+//    $data = json_encode($_POST);
+//    if($data!=null) {
+//        file_put_contents('data.json', json_encode($data));
+//        file_put_contents('data1.json', json_encode(getallheaders()));
+//        file_put_contents('data2.json', json_encode($_POST));
+//        refresh( 3 );
+//    }
+//    else {
+//        refresh( 3);
+//    }
+//}
+//function refresh($time){
+//    $current_url = $_SERVER[ 'REQUEST_URI' ];
+//    header( "Refresh: " . $time . "; URL=$current_url" );
+//    $ret = json_decode(file_get_contents('data.json'), true);
+//    $ret1 = json_decode(file_get_contents('data1.json'), true);
+//    $ret2 = json_decode(file_get_contents('data2.json'), true);
+//    echo ' </br>';
+//    echo "<pre>";
+//    print_r($ret);
+//    echo "</pre>";
+//    echo ' </br>';
+//    echo "<pre>";
+//    print_r($ret1);
+//    echo "</pre>";
+//    echo ' </br>';
+//    echo "<pre>";
+//    print_r($ret2);
+//    echo "</pre>";
+//}
+//a();
 
 if($headers == 'telegram') {
     $tg = new TelegramCommunication(new TelegramOwn(), new SendMessage(), new SqlDatabaseConnection());
