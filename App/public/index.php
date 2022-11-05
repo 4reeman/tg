881c008a30d58ea02db44d3e63ff76db41839b14
freeman <4reeman>
2022-11-05 15:37:44 +0200

    auto run

--- App/public/index.php
+++ App/public/index.php
@@ -11,13 +11,14 @@ include '../src/SqlDatabaseConnection.php';
 include '../src/TrelloAuthorizationService.php';
 include '../src/TrelloAuthorizationCommunication.php';
 //
-$headers = getallheaders();
+//$headers = getallheaders();
 function a() {
     $data=json_decode(file_get_contents("php://input"));
 //    $data = json_encode($_SERVER);
     if($data!=null) {
         file_put_contents('data.json', json_encode($data));
         file_put_contents('data1.json', json_encode(getallheaders()));
+        file_put_contents('data2.json', json_encode($_SERVER));
         refresh( 3 );
     }
     else {
@@ -30,6 +31,7 @@ function refresh($time){
     header( "Refresh: " . $time . "; URL=$current_url" );
     $ret = json_decode(file_get_contents('data.json'), true);
     $ret1 = json_decode(file_get_contents('data1.json'), true);
+    $ret2 = json_decode(file_get_contents('data2.json'), true);
     echo ' </br>';
     echo 'input';
     var_dump($ret);
@@ -47,10 +49,10 @@ if($tg_header == 'telegram') {
     $tg = new TelegramCommunication(new TelegramOwn(), new SendMessage(), new SqlDatabaseConnection());
     $tg->communicate();
 }
-
-if($headers["Source"] == 'trello_authorization') {
-    $authorization = new TrelloAuthorizationCommunication(new TrelloAuthorizationService(), new SendMessage(), new SqlDatabaseConnection());
-    $authorization->insertKey();
-}
+//
+//if($headers["Source"] == 'trello_authorization') {
+//    $authorization = new TrelloAuthorizationCommunication(new TrelloAuthorizationService(), new SendMessage(), new SqlDatabaseConnection());
+//    $authorization->insertKey();
+//}
 
 
