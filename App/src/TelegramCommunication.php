326507d44988d649e66391f3c66c91858ce42418
freeman <4reeman>
2022-11-09 12:24:48 +0200

    auto run

--- App/src/TelegramCommunication.php
+++ App/src/TelegramCommunication.php
@@ -104,8 +104,9 @@ class TelegramCommunication {
     public function trelloGetReport() {
 //        $get = file_get_contents("https://api.trello.com/1/members/me/boards?fields=name,url&key=ea3b9632108faebab5ffab2128e103ef&token=6a7c621b92d4d7e0edad96fcfaeefdade788c459ae91a82b957e5c0e565b4fa4");
         $getlis = file_get_contents("https://api.trello.com/1/boards/6351ebc9c751fa01e82f1390/lists?fields=name,url&key=ea3b9632108faebab5ffab2128e103ef&token=6a7c621b92d4d7e0edad96fcfaeefdade788c459ae91a82b957e5c0e565b4fa4");
-        $getlist = json_encode($getlis);
-        $arr = json_decode($getlist,true, 25, JSON_OBJECT_AS_ARRAY);
+        $decoded = json_decode($getlis);
+        //        $getlist = json_encode($getlis);
+//        $arr = json_decode($getlist,true, 25, JSON_OBJECT_AS_ARRAY);
 //        $lists = [];
 //        foreach($arr as $key => $value) {
 //            array_push($lists, $value['name']);
@@ -113,8 +114,8 @@ class TelegramCommunication {
 //        $result = implode($lists);
 //        $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => strval($get)]);
 //        $file = $arr[0]['name'];
-        file_put_contents('arr.txt', $arr);
-        $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => 'bla']);
+        file_put_contents('arr.txt', $decoded[0]->name);
+        $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => $decoded[0]->name]);
     }
 
 }
