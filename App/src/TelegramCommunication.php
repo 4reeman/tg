ef89f8653107a9081fe982cac15d3853d8ec8315
freeman <4reeman>
2022-11-09 02:55:29 +0200

    auto run

--- App/src/TelegramCommunication.php
+++ App/src/TelegramCommunication.php
@@ -104,12 +104,15 @@ class TelegramCommunication {
     public function trelloGetReport() {
         $get = file_get_contents("https://api.trello.com/1/members/me/boards?fields=name,url&key=ea3b9632108faebab5ffab2128e103ef&token=6a7c621b92d4d7e0edad96fcfaeefdade788c459ae91a82b957e5c0e565b4fa4");
         $getlis = file_get_contents("https://api.trello.com/1/boards/6351ebc9c751fa01e82f1390/lists?fields=name,url&key=ea3b9632108faebab5ffab2128e103ef&token=6a7c621b92d4d7e0edad96fcfaeefdade788c459ae91a82b957e5c0e565b4fa4");
-        $getlist = json_decode($getlis, true, 100, JSON_OBJECT_AS_ARRAY);
-        $str = implode("\n", $getlist);
-        file_put_contents('arr.txt', $str);
+//        $getlist = json_decode($getlis, true, 100, JSON_OBJECT_AS_ARRAY);
+        $ids = array();
+        foreach ($getlis as $obj){
+            $ids[] = $obj->name;
+        }
+
 //        file_put_contents('arr.txt', strval($getlist));
         $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => strval($get)]);
-        $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => strval($getlist)]);
+        $this->response->send('sendMessage', ['chat_id' => $this->data->getChatId(), 'text' => join(', ', $ids)]);
     }
 
 }
